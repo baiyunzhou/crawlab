d64a4d38beb6df869ebf810551ab29618a0467ac
zhoubaiyun <1181905571@qq.com>
2024-10-21 14:00:44 +0800

    feat: docker

--- build_backend.sh
+++
@@ -1,1 +0,0 @@
-docker build . --file=Dockerfile_backend -t zby/crawlab
---
+++ build_image.sh
@@ -0,0 +1,2 @@
+docker build . --file=Dockerfile_backend -t zby/crawlab_backend
+docker build . --file=Dockerfile_front -t zby/crawlab_front
