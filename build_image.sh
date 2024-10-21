04ea437b2aeb7a5bffa23ba9f742675f31bfdb45
zhoubaiyun <1181905571@qq.com>
2024-10-21 17:19:35 +0800

    feat: docker

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,2 @@
 docker build . --file=Dockerfile_backend -t zby/crawlab_backend
-docker build . --file=Dockerfile_frontend -t zby/crawlab_front
+docker build . --file=Dockerfile_frontend -t zby/crawlab_frontend
